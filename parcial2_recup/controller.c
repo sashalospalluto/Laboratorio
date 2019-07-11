acbc30d55e1eafb9e1ae3188a2264515f5e6d28c
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2019-07-11 17:37:49 -0300

    Add files via upload

---
+++ parcial2_recup/controller.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "controller.h"
+//#include "LinkedList.h"
+#include "parser.h"
+#include "registro.h"
+
+/** \brief Carga los datos del registro desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param this LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromText(char* path, LinkedList* this)
+{
+    int returnRet=-1;
+    FILE* pFile;
+    pFile = fopen(path,"r");
+    returnRet=parser_registroFromText(pFile, this);
+    fclose(pFile);
+    return returnRet;
+}
+
+int controller_saveAsText(char* path, LinkedList* this)
+{
+    FILE* pf;
+    int returnRet=-1;
+    int fotosTotales=0;
+    int cantVentas1=0;
+    int cantVentas2=0;
+    int totalPolaroid=0;
+    pf=fopen(path,"w");
+
+    if(this!=NULL && pf!=NULL)
+    {
+        fotosTotales = ll_count(this,cuentaFotos);
+        cantVentas1 = ll_count(this, ventasUno);
+        cantVentas2 = ll_count(this, ventasDos);
+        totalPolaroid = ll_count(this, cuentaPolaroid);
+
+        fprintf(pf,"Registro\n");
+        fprintf(pf,"- Cantidad de fotos reveladas: %d\n",fotosTotales);
+        fprintf(pf,"- Cantidad de ventas mayores a $150: %d\n",cantVentas1);
+        fprintf(pf,"- Cantidad de ventas mayores a $300: %d\n",cantVentas2);
+        fprintf(pf,"- Cantidad de fotos polaroids reveladas: %d\n",totalPolaroid);
+        fclose(pf);
+        returnRet=0;
+    }
+    return returnRet;
+}
+
+int controller_printAllRegistros(LinkedList* this)
+{
+    int returnRet=-1;
+    int i,len;
+    len=ll_len(this);
+    if(this!=NULL && len>0)
+    {
+        for(i=0;i<len;i++)
+        {
+            controller_printRegistro(this,i);
+            returnRet=0;
+        }
+    }
+    return returnRet;
+}
+
+int controller_printRegistro(LinkedList* this, int index)
+{
+    Registro* registroAux;
+    int id;
+    char fecha[100];
+    char tipoFoto[100];
+    int cantidad;
+    float precio;
+    char cuit[100];
+    int returnRet=-1;
+    if(this!=NULL && index>=0 && index<ll_len(this))
+    {
+        registroAux=(Registro*)ll_get(this,index);
+        registro_getId(registroAux,&id);
+        registro_getFecha(registroAux,fecha);
+        registro_getFoto(registroAux,tipoFoto);
+        registro_getCantidad(registroAux,&cantidad);
+        registro_getPrecio(registroAux,&precio);
+        registro_getCuit(registroAux,cuit);
+        printf("%d , %s , %s , %d , %.2f , %s\n",id,fecha,tipoFoto,cantidad,precio,cuit);
+        returnRet=0;
+    }
+    return returnRet;
+}
---
+++ parcial2_recup/registro.c
@@ -0,0 +1,309 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "registro.h"
+#include "LinkedList.h"
+#include "utn.h"
+
+Registro* registro_new()
+{
+    return (Registro*) malloc(sizeof (Registro));
+}
+
+Registro* registro_newParametros(char* id,char* cantidad,char* precio,char* fecha, char* tipoFoto,char* cuit)
+{
+    Registro* aux;
+    aux = registro_new();
+
+    if(aux != NULL)
+    {
+        registro_setIdChar(aux , id);
+        registro_setCantidadChar(aux, cantidad);
+        registro_setPrecioChar(aux, precio);
+        registro_setFecha(aux, fecha);
+        registro_setFoto(aux, tipoFoto);
+        registro_setCuit(aux, cuit);
+    }
+    return aux;
+}
+
+int registro_setId(Registro* this,int id)
+{
+    int returnRet =-1;
+
+    if(this != NULL && id>0)
+    {
+        this->id =id;
+        returnRet = 0;
+    }
+    return returnRet;
+}
+
+int registro_setIdChar(Registro* this,char* idChar)
+{
+    int returnRet=-1;
+    int idAux;
+    if(this!=NULL && idChar!=NULL)
+    {
+        if(isValidNumber(idChar)==1)
+        {
+            idAux = atoi(idChar);
+            if(registro_setId(this,idAux)==0)
+            {
+                returnRet=0;
+            }
+        }
+    }
+    return returnRet;
+}
+
+int registro_getId(Registro* this,int* id)
+{
+    int returnRet=-1;
+
+    if(this != NULL && id != NULL)
+    {
+        *id= this->id;
+        returnRet=0;
+    }
+    return returnRet;
+}
+
+int registro_setCantidad(Registro* this,int cant)
+{
+    int returnRet =-1;
+
+    if(this != NULL && cant>0)
+    {
+        this->cantidad =cant;
+        returnRet = 0;
+    }
+    return returnRet;
+}
+
+int registro_setCantidadChar(Registro* this,char* cantidadChar)
+{
+    int returnRet=-1;
+    int cantidadAux;
+    if(this!=NULL && cantidadChar!=NULL)
+    {
+        if(isValidNumber(cantidadChar)==1)
+        {
+            cantidadAux = atoi(cantidadChar);
+            if(registro_setCantidad(this,cantidadAux)==0)
+            {
+                returnRet=0;
+            }
+        }
+    }
+    return returnRet;
+}
+
+int registro_getCantidad(Registro* this,int* cant)
+{
+    int returnRet=-1;
+
+    if(this != NULL && cant != NULL)
+    {
+        *cant= this->cantidad;
+        returnRet=0;
+    }
+    return returnRet;
+}
+
+int registro_setFecha(Registro* this,char* fecha)
+{
+    int returnRet =-1;
+
+    if(this != NULL)
+    {
+        if (utn_validaFecha(fecha)==0)
+        {
+            strcpy (this->fecha, fecha);
+            returnRet=0;
+        }
+    }
+    return returnRet;
+}
+
+int registro_getFecha(Registro* this,char* fecha)
+{
+    int returnRet =-1;
+
+    if(this != NULL && fecha != NULL)
+    {
+        strcpy(fecha, this->fecha);
+        returnRet =0;
+    }
+    return returnRet;
+}
+
+int registro_setPrecio(Registro* this, float precio)
+{
+    int returnRet =-1;
+
+    if(this != NULL && precio>0)
+    {
+        this->precio =precio;
+        returnRet = 0;
+    }
+    return returnRet;
+}
+
+int registro_setPrecioChar(Registro* this,char* precioChar)
+{
+    int returnRet=-1;
+    float precioAux;
+    if(this!=NULL && precioChar!=NULL)
+    {
+        if(isValidFloatNumber(precioChar)==1)
+        {
+            precioAux = atof(precioChar);
+            if(registro_setPrecio(this,precioAux)==0)
+            {
+                returnRet=0;
+            }
+        }
+
+    }
+    return returnRet;
+}
+
+int registro_getPrecio(Registro* this,float* precio)
+{
+    int returnRet=-1;
+
+    if(this != NULL && precio != NULL)
+    {
+        *precio= this->precio;
+        returnRet=0;
+    }
+    return returnRet;
+}
+
+int registro_setFoto(Registro* this,char* foto)
+{
+    int returnRet =-1;
+
+    if(this != NULL)
+    {
+         strcpy (this->tipoFoto, foto);
+    }
+    return returnRet;
+}
+
+int registro_getFoto(Registro* this,char* foto)
+{
+    int returnRet =-1;
+
+    if(this != NULL && foto != NULL)
+    {
+        strcpy(foto, this->tipoFoto);
+        returnRet =1;
+    }
+    return returnRet;
+}
+
+int registro_setCuit(Registro* this,char* cuit)
+{
+    int returnRet =-1;
+
+    if(this != NULL)
+    {
+         strcpy (this->cuit, cuit);
+         returnRet=0;
+    }
+    return returnRet;
+}
+
+int registro_getCuit(Registro* this,char* cuit)
+{
+    int returnRet =-1;
+
+    if(this != NULL && cuit != NULL)
+    {
+        strcpy(cuit, this->cuit);
+        returnRet =0;
+    }
+    return returnRet;
+}
+
+int registro_delete(Registro* this)
+{
+    int ret=-1;
+    if(this!=NULL)
+    {
+        free(this);
+        ret=0;
+    }
+    return ret;
+}
+
+int cuentaFotos(void* element)
+{
+    int returnRet=0;
+
+    Registro* foto;
+    foto = (Registro*)element;
+
+    returnRet = foto->cantidad;
+
+    return returnRet;
+}
+
+int ventasUno (void* element)
+{
+    int returnRet =0;
+    Registro* foto;
+    float total;
+    int cantidadFotos;
+    float precio;
+    foto = (Registro*)element;
+
+    cantidadFotos = foto->cantidad;
+    precio = foto->precio;
+    total = cantidadFotos * precio;
+
+    if(total > 150)
+    {
+        returnRet =1;
+    }
+    return returnRet;
+}
+
+int ventasDos (void* element)
+{
+    int returnRet =0;
+    Registro* foto;
+    float total;
+    int cantidadFotos;
+    float precio;
+    foto = (Registro*)element;
+
+    cantidadFotos = foto->cantidad;
+    precio = foto->precio;
+    total = cantidadFotos * precio;
+
+    if(total > 250)
+    {
+        returnRet =1;
+    }
+    return returnRet;
+}
+
+int cuentaPolaroid (void* element)
+{
+    int returnRet=0;
+    Registro* foto;
+    foto = (Registro*) element;
+
+    if (strcmp(foto->tipoFoto, "POLAROID_11x9")==0)
+    {
+        returnRet=1;
+    }
+    if (strcmp(foto->tipoFoto, "POLAROID_10x10")==0)
+    {
+        returnRet=1;
+    }
+    return returnRet;
+}
